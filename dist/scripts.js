af2c3d46c272a684b8e61738c71b614bc3806049
Anthony Camacho <anthony.g.camacho@gmail.com>
2022-02-25 09:34:50 -0800

    building

---
+++ dist/scripts.js
@@ -0,0 +1,25 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define('main', factory) :
+	factory();
+}((function () { 'use strict';
+
+	// import './../scss/main.scss';
+
+	// const { add } = require('./test');
+
+	// import { add } from './test'
+
+	// console.log(add(2, 7))
+
+	console.log('fart');
+
+})));
+
+(function (factory) {
+	typeof define === 'function' && define.amd ? define('test', factory) :
+	factory();
+}((function () { 'use strict';
+
+
+
+})));
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,42 @@
 'use strict'
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')(require('sass'))
-var concat = require('gulp-concat')
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const gpBetterRollup = require('gulp-better-rollup')
+
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+
+const babel = require('gulp-babel')
 
 sass.compiler = require('node-sass')
+
+gulp.task('jsbuild', async function () {
+  return gulp
+    .src('./assets/js/**/*.js')
+    .pipe(
+      gpBetterRollup(
+        {
+          plugins: [
+            babel({
+              presets: ['@babel/preset-env'],
+            }),
+            resolve(),
+            commonjs(),
+          ],
+        },
+        'umd',
+      ),
+    )
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist'))
+  // .pipe(rename('scripts.min.js'))
+  // .pipe(uglify())
+  // .pipe(gulp.dest('./public/js/'))
+})
 
 gulp.task('sass', function () {
   return (
@@ -17,9 +49,15 @@ gulp.task('sass', function () {
   )
 })
 
-gulp.task('sass:watch', function () {
+gulp.task('watch:sass', function () {
   gulp.watch('./assets/scss/**/*.scss', gulp.series('sass'))
 })
+
+gulp.task('watch:js', function () {
+  gulp.watch('./assets/js/**/*.js', gulp.series('jsbuild'))
+})
+
+// gulp.task('watch', gulp.series('watch'))
 
 // Default Task
 // gulp.task('default', gulp.parallel('sass', 'sass:watch'))
